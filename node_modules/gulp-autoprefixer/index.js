bbb7f3e8b8ab00fd86f7a87f9a4a139d16208d50
Antoine Boucher <antoine.bou13@gmail.com>
2024-01-11 21:30:46 +0000

    add more stuff

--- node_modules/gulp-autoprefixer/index.js
+++ node_modules/gulp-autoprefixer/index.js
@@ -18,43 +18,39 @@ module.exports = options => {
 			return;
 		}
 
-		(async () => {
-			try {
-				const result = await postcss(autoprefixer(options)).process(file.contents.toString(), {
-					map: file.sourceMap ? {annotation: false} : false,
-					from: file.path,
-					to: file.path
-				});
+		postcss(autoprefixer(options)).process(file.contents.toString(), {
+			map: file.sourceMap ? {annotation: false} : false,
+			from: file.path,
+			to: file.path
+		}).then(result => {
+			file.contents = Buffer.from(result.css);
 
-				file.contents = Buffer.from(result.css);
-
-				if (result.map && file.sourceMap) {
-					const map = result.map.toJSON();
-					map.file = file.relative;
-					map.sources = map.sources.map(() => file.relative);
-					applySourceMap(file, map);
-				}
-
-				const warnings = result.warnings();
-
-				if (warnings.length > 0) {
-					fancyLog('gulp-autoprefixer:', '\n  ' + warnings.join('\n  '));
-				}
-
-				setImmediate(callback, null, file);
-			} catch (error) {
-				const cssError = error.name === 'CssSyntaxError';
-
-				if (cssError) {
-					error.message += error.showSourceCode();
-				}
-
-				// Prevent stream unhandled exception from being suppressed by Promise
-				setImmediate(callback, new PluginError('gulp-autoprefixer', error, {
-					fileName: file.path,
-					showStack: !cssError
-				}));
+			if (result.map && file.sourceMap) {
+				const map = result.map.toJSON();
+				map.file = file.relative;
+				map.sources = map.sources.map(() => file.relative);
+				applySourceMap(file, map);
 			}
-		})();
+
+			const warnings = result.warnings();
+
+			if (warnings.length > 0) {
+				fancyLog('gulp-autoprefixer:', '\n  ' + warnings.join('\n  '));
+			}
+
+			setImmediate(callback, null, file);
+		}).catch(error => {
+			const cssError = error.name === 'CssSyntaxError';
+
+			if (cssError) {
+				error.message += error.showSourceCode();
+			}
+
+			// Prevent stream unhandled exception from being suppressed by Promise
+			setImmediate(callback, new PluginError('gulp-autoprefixer', error, {
+				fileName: file.path,
+				showStack: !cssError
+			}));
+		});
 	});
 };
