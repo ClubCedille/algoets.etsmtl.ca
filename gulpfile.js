860fa84344581b2171c4e6c86c3239f1d2ab34b9
Antoine Boucher <antoine.bou13@gmail.com>
2024-01-26 22:48:12 +0000

    init workshop

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var path = {
   src: {
     html: "source/*.html",
     blog: "source/blog/*.html",
+    files: "source/files/*.pdf",
     others: "source/*.+(php|ico|png)",
     htminc: "source/partials/**/*.htm",
     incdir: "source/partials/",
@@ -147,6 +148,18 @@ gulp.task("others:build", function () {
   return gulp.src(path.src.others).pipe(gulp.dest(path.build.dirDev));
 });
 
+// PDF Files
+gulp.task("files:build", function () {
+    return gulp
+        .src(path.src.files)
+        .pipe(gulp.dest(path.build.dirDev + "files/"))
+        .pipe(
+            bs.reload({
+                stream: true,
+            })
+        );
+});
+
 // Clean Build Folder
 gulp.task("clean", function (cb) {
   rimraf("./theme", cb);
@@ -161,6 +174,7 @@ gulp.task("watch:build", function () {
   gulp.watch(path.src.js, gulp.series("js:build"));
   gulp.watch(path.src.images, gulp.series("images:build"));
   gulp.watch(path.src.plugins, gulp.series("plugins:build"));
+  gulp.watch(path.src.files, gulp.series("files:build"));
 });
 
 // Dev Task
@@ -175,6 +189,7 @@ gulp.task(
     "images:build",
     "plugins:build",
     "others:build",
+    "files:build",
     gulp.parallel("watch:build", function () {
       bs.init({
         server: {
@@ -194,6 +209,8 @@ gulp.task(
     "js:build",
     "scss:build",
     "images:build",
-    "plugins:build"
+    "plugins:build",
+    "others:build",
+    "files:build"
   )
 );
